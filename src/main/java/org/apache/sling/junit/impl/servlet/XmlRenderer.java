37b25d2feff30abe9e8c4b48e04b9da689acb2ef
Robert Munteanu <rombert@apache.org>
2025-03-28 00:04:05 +0100

    SLING-12722 - Enable secure XML processing in XmlRenderer

--- src/main/java/org/apache/sling/junit/impl/servlet/XmlRenderer.java
+++ src/main/java/org/apache/sling/junit/impl/servlet/XmlRenderer.java
@@ -29,12 +29,14 @@ import java.util.Hashtable;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.XMLConstants;
 
 import junit.runner.BaseTestRunner;
 
@@ -279,7 +281,9 @@ public class XmlRenderer extends RunListener implements Renderer, RendererFactor
 	 */
 	public static DocumentBuilder getDocumentBuilder() {
 		try {
-			return DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+			return factory.newDocumentBuilder();
 		} catch (Exception exc) {
 			throw new ExceptionInInitializerError(exc);
 		}
@@ -293,6 +297,7 @@ public class XmlRenderer extends RunListener implements Renderer, RendererFactor
 	public static String getStringFromElement(Element element) {
 		try {
 			TransformerFactory tf = TransformerFactory.newInstance();
+			tf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
 			Transformer trans = tf.newTransformer();
 			StringWriter sw = new StringWriter();
 			trans.transform(new DOMSource(element), new StreamResult(sw));
